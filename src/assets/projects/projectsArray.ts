beb01b634418d95d1b61f1a4e605fd7b43e3b4a1
Shiva <itsshivahere@outlook.com>
2024-03-14 02:23:41 +0530

    Update projectsArray.ts

--- src/assets/projects/projectsArray.ts
+++ src/assets/projects/projectsArray.ts
@@ -8,6 +8,14 @@ export const projectArray = [
         hostedLink: "https://www.animetrix.xyz/",
         githubLink: "https://www.github.com/shivabhattacharjee/animetrix-next",
     },
+ {
+        name: "Quibble",
+        description: "Quibble, a content generation bot, simplifies and elevates your creative process developed in NEXTJS 14 and TYPESCRIPT. Whether crafting engaging blog posts, soulful song lyrics, or attention-grabbing Insta captions, Quibble's advanced algorithms seamlessly transform your ideas into polished, professional content.",
+        image: quibbleimg,
+        type: "open source",
+        hostedLink: "https://quibble.theshiva.xyz",
+        githubLink: "https://www.github.com/shivabhattacharjee/quibble",
+    },
     {
         name: "Muxik",
         description: "Muxik is a music streaming and downloading platform developed on ReactJs. It provides users with a wide variety of songs, albums, playlists, and artists across different genres. Users can enjoy a seamless and immersive music listening experience through the platform.",
@@ -31,13 +39,5 @@ export const projectArray = [
         type: "open source",
         hostedLink: "",
         githubLink: "https://www.github.com/shivabhattacharjee/synthia",
-    },
-    {
-        name: "Quibble",
-        description: "Quibble, a content generation bot, simplifies and elevates your creative process developed in NEXTJS 14 and TYPESCRIPT. Whether crafting engaging blog posts, soulful song lyrics, or attention-grabbing Insta captions, Quibble's advanced algorithms seamlessly transform your ideas into polished, professional content.",
-        image: quibbleimg,
-        type: "open source",
-        hostedLink: "https://quibble.theshiva.xyz",
-        githubLink: "https://www.github.com/shivabhattacharjee/quibble",
-    },
+    }
 ];
