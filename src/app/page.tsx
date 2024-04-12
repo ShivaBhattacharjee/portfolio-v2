2b73e0a5416fddec047cc4014cde52f035f7b8bb
ShivaBhattacharjee <shivaxwarrior@gmail.com>
2024-04-12 16:20:06 +0530

    well alarm clock 3d icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { GitHubLogoIcon, InstagramLogoIcon, TwitterLogoIcon } from "@radix-ui/react-icons";
 import { ChevronRight, Info, MailPlus, MouseIcon, Terminal } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 
+import clock from "@/assets/clock.svg";
 import { projectArray } from "@/assets/projects/projectsArray";
 import Card from "@/components/projects/card";
 import Contact from "@/components/shared/Contact";
 import Skills from "@/components/skills/Skills";
 import Terminalcomp from "@/components/terminal/Terminalcomp";
 import TypingAnimation from "@/components/TypingAnimation";
-
 const page = () => {
     return (
         <section className="lg:max-w-[50%] mt-20 md:mt-36 md:max-w-[90%] relative md:m-auto p-4 flex flex-col min-h-screen">
@@ -18,9 +19,10 @@ const page = () => {
                     <Terminal size={17} />
                     Hello World
                 </span>
-                <h1 className="text-4xl relative md:text-7xl lg:text-8xl font-bold ">
-                    Hey, I&apos;m Shiva <span className=" absolute bottom-0 text-8xl lg:text-[12rem] ml-2 animate-pulse">.</span>
-                </h1>
+                <div className="flex items-center gap-3">
+                    <h1 className="text-4xl flex gap-12 items-center flex-row md:text-7xl lg:text-8xl font-bold ">Hey, I&apos;m Shiva</h1>
+                    <Image src={clock} alt="coffeCup" className="w-12 rotate-12 h-12 lg:w-16 lg:h-16" />
+                </div>
                 <h1 className=" font-bold lg:text-3xl">
                     I am a <TypingAnimation />
                 </h1>
