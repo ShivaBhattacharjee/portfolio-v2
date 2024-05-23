b82d59d9f5104eb9841a4bca3a0a49b61e0f6599
ShivaBhattacharjee <shivaxwarrior@gmail.com>
2024-05-24 02:39:02 +0530

    add: npx btn

--- src/components/CopyCmd.tsx
+++ src/components/CopyCmd.tsx
@@ -30,7 +30,7 @@ const CopyCmd = () => {
             });
     };
     return (
-        <div onClick={handleCopyToClipboard} className="homepagecontactbtn">
+        <div onClick={handleCopyToClipboard} className="homepagecontactbtn pointer-events-none">
             {copied ? <ClipboardCheck /> : <Clipboard />}
             npx shivadev
         </div>
