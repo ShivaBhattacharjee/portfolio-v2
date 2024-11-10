85679a71a5c622ba19c296e42b2bc3bd6b987e31
Shiva <shivaxwarrior@gmail.com>
2024-11-11 02:20:39 +0530

    Update projectsArray.ts

--- src/assets/projects/projectsArray.ts
+++ src/assets/projects/projectsArray.ts
@@ -5,7 +5,7 @@ export const projectArray = [
         description: "Anime Trix is a full stack anime streaming / downloading site developed in NEXTJS 13.5 and TYPESCRIPT that offers a vast collection of anime shows for streaming and downloading. With a user-friendly interface, you can easily find your favorite anime shows and watch them in high-definition quality.",
         image: animetriximg,
         type: "open source",
-        hostedLink: "https://www.animetrix.xyz/",
+        hostedLink: "",
         githubLink: "https://www.github.com/shivabhattacharjee/animetrix-next",
     },
     {
